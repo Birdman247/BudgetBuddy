c3187c0fe9f31a65d796d6506d30df28481280d0
ZanderBird <157409786+Birdman247@users.noreply.github.com>
2025-05-15 23:04:41 -0500

    Initialize Transaction Class

---
+++ Transaction.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+
+public class Transaction {
+    private double amount;
+    private String type; 
+    private String category;
+    private Date date;
+
+    public Transaction(double amount, String type, String category, Date date) {
+        this.amount = amount;
+        this.type = type;
+        this.category = category;
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
